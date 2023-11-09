070e10b43f4fcdee5c0026ca7f18ba77d6478e72
jojurg <jojurg@taltech.com>
2023-11-10 01:50:26 +0200

    refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crossterm::{
     event::{self, Event::Key, KeyCode::Char},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand,
 };
 
 use ratatui::{
@@ -17,49 +17,68 @@ struct App {
     should_quit: bool,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // startup: Enalbe raw mode for the terminal,  giving us fine control over
-    // the user input
-    crossterm::terminal::enable_raw_mode()?;
-    crossterm::execute!(std::io::stderr(), crossterm::terminal::EnterAlternateScreen)?;
 
-    // initialize the terminal backend using crossterm
-    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+fn startup() -> Result<()> {
+    enable_raw_mode();
+    execute!(std::io::stderr(), EnterAlternateScreen)?;
+    Ok(())
+}
 
-    // define our counter variable
-    // this is the state of our application
-    let mut counter = 0;
+fn shutdown() -> Result<()> {
+    execute!(std::io::stderr(), LeaveAlternateScreen)?;
+    disable_raw_mode();
+    Ok(())
+}
 
-    // main app loop
-    loop {
-        // render UI
-        terminal.draw(|f| {
-            f.render_widget(Paragraph::new(format!("Counter: {counter}")), f.size());
-        })?;
+fn ui(app: &App, f: &mut Frame<'_>) {
+    f.render_widget(Paragraph::new(format!("Counter: {}", app.counter)), f.size());
+}
 
-        // check for user input every 250 ms
-        if crossterm::event::poll(std::time::Duration::from_millis(250))? {
-            // handle key events
-            if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
-                
-                // this speficic check for KeyEventKind::Press is here so that on windows
-                // we wouldn't get 2 events for every press, because on windows we other-
-                // wise would send the same event twice - on Press and on Release
-                if key.kind == crossterm::event::KeyEventKind::Press {
-                    match key.code {
-                        crossterm::event::KeyCode::Char('j') => counter += 1,
-                        crossterm::event::KeyCode::Char('k') => counter -= 1,
-                        crossterm::event::KeyCode::Char('q') => break,
-                        _ => {},
-                    }
+fn update(app: &mut App) ->  Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+        if let Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    Char('j') => app.counter += 1,
+                    Char('k') => app.counter -= 1,
+                    Char('q') => app.should_quit = true,
+                    _ => {},
                 }
             }
         }
     }
+    Ok(())
+}
 
-    // shutdown: reset terminal back to original state
-    crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)?;
-    crossterm::terminal::disable_raw_mode()?;
+fn run() -> Result<()> {
+    // ratatui terminal
+    let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
 
+    // application state
+    let mut app = App { counter: 0, should_quit: false };
+
+    loop {
+        t.draw(|f| {
+            ui(&app, f);
+        })?;
+
+        // application update
+        update(&mut app)?;
+
+        // application exit
+        if app.should_quit {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+
+fn main() -> Result<()> {
+    startup()?;
+    let result = run()?;
+    shutdown()?;
+    result?;
     Ok(())
 }
