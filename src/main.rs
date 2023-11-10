897e3d9f73c8b14c8e23a107dc64c7d5d0587d1b
jojurg <jojurg@taltech.com>
2023-11-10 16:30:28 +0200

    refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,4 @@
-use crossterm::{
-    event::{self, Event::Key, KeyCode::Char},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-
-use ratatui::{
-    prelude::{CrosstermBackend, Terminal},
-    widgets::Paragraph,
-};
-
-use anyhow::Result;
-pub type Frame<'a> = ratatui::Frame<'a>;
-
-/// declare our files as modules
-/// Application
+/// Main application
 pub mod app;
 
 /// Terminal events handler
@@ -28,73 +13,33 @@ pub mod tui;
 /// Application updater
 pub mod update;
 
-struct App {
-    counter: i64,
-    should_quit: bool,
-}
-
-
-fn startup() -> Result<()> {
-    enable_raw_mode();
-    execute!(std::io::stderr(), EnterAlternateScreen)?;
-    Ok(())
-}
-
-fn shutdown() -> Result<()> {
-    execute!(std::io::stderr(), LeaveAlternateScreen)?;
-    disable_raw_mode();
-    Ok(())
-}
-
-fn ui(app: &App, f: &mut Frame<'_>) {
-    f.render_widget(Paragraph::new(format!("Counter: {}", app.counter)), f.size());
-}
-
-fn update(app: &mut App) ->  Result<()> {
-    if event::poll(std::time::Duration::from_millis(10000))? {
-        if let Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press {
-                match key.code {
-                    Char('j') => app.counter += 1,
-                    Char('k') => app.counter -= 1,
-                    Char('q') => app.should_quit = true,
-                    _ => {},
-                }
-            }
-        }
-    }
-    Ok(())
-}
-
-fn run() -> Result<()> {
-    // ratatui terminal
-    let mut t = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
-
-    // application state
-    let mut app = App { counter: 0, should_quit: false };
-
-    loop {
-        t.draw(|f| {
-            ui(&app, f);
-        })?;
-
-        // application update
-        update(&mut app)?;
-
-        // application exit
-        if app.should_quit {
-            break;
-        }
-    }
-
-    Ok(())
-}
+use anyhow::Result;
+use app::App;
+use event::{Event, EventHandler};
+use ratatui::{backend::CrosstermBackend, Terminal};
+use tui::Tui;
+use update::update;
 
 
 fn main() -> Result<()> {
-    startup()?;
-    let result = run()?;
-    shutdown()?;
-    result;
+    let mut app = App::new();
+
+    let backend = CrosstermBackend::new(std::io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.enter()?;
+
+    while !app.should_quit {
+        tui.draw(&mut app)?;
+        match tui.events.next()? {
+            Event::Tick => {},
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {},
+            Event::Resize(_, _) => {},
+        };
+    }
+
+    tui.exit()?;
     Ok(())
 }
