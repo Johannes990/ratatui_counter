19d457e0b042a9ac0c8c1983de90453143e011d5
jojurg <jojurg@taltech.com>
2023-11-10 16:05:52 +0200

    implemented Tui functions

--- src/tui.rs
+++ src/tui.rs
@@ -20,3 +20,30 @@ pub struct Tui {
     terminal: CrosstermTerminal,
     pub events: EventHandler,
 }
+
+impl Tui {
+    // Construct new instance of 'Tui'
+    pub fn new(terminal: CrosstermTerminal, events: EventHandler) -> Self {
+        Self { terminal, events }
+    }
+
+    /// Initialize the terminal interface
+    /// 
+    /// it enables the raw mode and sets terminal properties
+    pub fn enter(&mut self) -> Result<()> {
+        terminal::enable_raw_mode();
+        crossterm::execute!(io::stderr(), EnterAlternateScreen, EnableMouseCapture)?;
+
+        // define a custom panic hook to reset the terminal properties
+        // this way, you won't have your terminal messed up if an unexpected error happens
+        let panic_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic| {
+            Self::reset().expect("failed to reset the terminal");
+            panic_hook(panic);
+        }));
+
+        self.terminal.hide_cursor();
+        self.terminal.clear();
+        Ok(())
+    }
+}
